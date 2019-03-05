126350014d7e21bdeb56cc18cdc10441c33d6b3b
‘daiyanping’ <917311025@qq.com>
2019-03-05 11:56:33 +0800

    张三和李四同时修改一个文件

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args ) {
 
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!2" );
+        System.out.println( "张三的改动" );
+        System.out.println( "李斯的改动!2" );
 
     }
 }
